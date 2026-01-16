bc660629c867d1c0b72efbe1b1863ee5fde95284
Bsok <bsok@example.com>
2026-01-16 15:59:28 +0100

    Spring Boot project with JPA repository

---
+++ src/main/java/com/example/demo/Bohdan54660Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bohdan54660Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bohdan54660Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/BohdanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BohdanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BohdanApplication.class, args);
+	}
+
+}
